8dcd623c40ac4c6b5d8269950aee64190ce7caa7
Chirag Kapil <your.email@example.com>
2024-12-15 11:42:37 +0530

    shortner

--- Api/script.js
+++ Api/script.js
@@ -32,7 +32,7 @@ app.get("/:shortCode", (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
